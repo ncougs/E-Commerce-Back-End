350ef65c8682daf133bbd200d10e192450278c2b
ncougs <ncougan@gmail.com>
2021-08-21 12:08:43 +1000

    Adding category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,68 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  const data = await Category.findAll({ include: Product });
+  res.json(data);
+
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  const id = req.params.id;
+  const data = await Category.findByPk(id, { include: Product });
+  res.json(data);
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  const { category_name } = req.body;
+
+  if(category_name) {
+    const newData = {
+      category_name: category_name
+    };
+
+  const addData = await Category.create(newData);
+
+  res.json(addData);
+  }
+  else {
+    res.send(`Incorrect fields in post request`)
+  };
+
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  const id = req.params.id;
+  const { category_name } = req.body;
+
+  const update = await Category.update({ category_name: category_name }, {
+    where: {
+      id: id
+    }
+  });
+
+  res.json(await Category.findByPk(id));
+
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  const id = req.params.id;
+
+  const destroy = await Category.destroy({
+    where: {
+      id: id
+    }
+  });
+
+  res.sendStatus(200);
+
 });
 
 module.exports = router;
