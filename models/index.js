20b07ea05869bbfc9733ff15cd32b8869991736b
ncougs <ncougan@gmail.com>
2021-08-23 16:59:22 +1000

    Updaing product foreign key

--- models/index.js
+++ models/index.js
@@ -7,12 +7,13 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
+  allowNull: true,
+  onDelete: 'CASCADE'
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'category_id',
-  onDelete: 'CASCADE',
+  foreignKey: 'category_id'
 });
 
 // Products belongToMany Tags (through ProductTag)
